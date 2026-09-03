2ec7bc55f00e4b2f2bb021ea6ac533de3ee107a6
Jamie Le Souef <jlesouef@icloud.com>
2026-09-03 10:52:36 +1000

    Add new documentation for project reset (#6)
    * project reset
    
    * added new documentation
    
    ---------
    
    Co-authored-by: Jamie Le Souef <j.lesouef@easygo.io>

---
+++ tvOS/tests/mixtape_tvTests.swift
@@ -0,0 +1,19 @@
+//
+//  mixtape_tvTests.swift
+//  mixtape.tvTests
+//
+//  Created by Jamie Le Souef on 3/9/2026.
+//
+
+import Testing
+@testable import mixtape_tv
+
+struct mixtape_tvTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
